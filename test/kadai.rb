1fac5794dc1f1df2ad0a08b39c2f2000b1265cdc
no5td <no5@no5td.com>
2017-07-10 09:03:54 +0900

    third commit

--- test/kadai.rb
+++ test/kadai.rb
@@ -1,10 +1,12 @@
 ary = (1..100).to_a
 p ary
+p '----------------------------------------------------------------'
 array = []
 ary.each do |i|
   array << i * 100
 end
 p array
+p '----------------------------------------------------------------'
 eiji = ("a".."z").to_a
 eijisu = Array.new(26,0)
 string = "I am a Ruby hacker"
@@ -16,12 +18,15 @@ while moji = string.slice!(0)
   end
 end
 for i in 0...26
-  print eiji[i], ":", "★" * eijisu[i], "\n"
+  if eijisu[i] != 0
+    print eiji[i], ":", "★" * eijisu[i], "\n"
+  end
 end
+p '----------------------------------------------------------------'
 suji = 0
-string = "二億六千百五十二万七千百二十三"
+string = "二億六千百十二万七千百二十三"
 kansuji = {"零" => 0,"一" => 1,"二" => 2,"三" => 3,"四" => 4,"五" => 5,"六" => 6,"七" => 7,"八" => 8,"九" => 9}
-kansuji_zyu = {"十" => 1,"百" => 2,"千" => 3, "万" => 4, "億" => 8, "兆" => 12}
+kansuji_zyu = {"十" => 1,"百" => 2,"千" => 3, "万" => 4, "億" => 4, "兆" => 4}
 reverseString = string.reverse
 renzoku = false
 suuti = 0
@@ -34,23 +39,20 @@ while moji = reverseString.slice!(0)
     else
       suuti += zyu
     end
-    print "st", zyuzyou, "\n"
     if zyuzyou < kansuji_zyu["万"]
       zyuzyou = kansuji_zyu[moji]
-    elsif zyuzyou < kansuji_zyu["億"]
+    elsif zyuzyou < kansuji_zyu["億"] + kansuji_zyu["万"]
       zyuzyou = kansuji_zyu[moji] + kansuji_zyu["万"]
-    elsif zyuzyou == kansuji_zyu["億"]
-      zyuzyou = kansuji_zyu[moji] - kansuji_zyu["万"]
-    elsif zyuzyou < kansuji_zyu["兆"]
-      zyuzyou = kansuji_zyu[moji] + 8
+    elsif zyuzyou < kansuji_zyu["兆"] + kansuji_zyu["億"] + kansuji_zyu["万"]
+      zyuzyou = kansuji_zyu[moji] + kansuji_zyu["億"] + kansuji_zyu["万"]
     else
-      zyuzyou = kansuji_zyu[moji] + 12
+      zyuzyou = kansuji_zyu[moji] + kansuji_zyu["兆"] + kansuji_zyu["億"] + kansuji_zyu["万"]
     end
-    p zyuzyou
     zyu = 10 ** zyuzyou
   else
     renzoku = false
     suuti += (kansuji[moji] * zyu)
-    p suuti
   end
 end
+p string
+p suuti
