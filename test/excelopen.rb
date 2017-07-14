e7aa2ede186e278fff983c85404ad91c64fc6d58
no5td <no5@no5td.com>
2017-07-14 16:29:26 +0900

    excelopen commit

--- test/excelopen.rb
+++ test/excelopen.rb
@@ -1,32 +1,43 @@
 require 'win32ole'
 xl = WIN32OLE.new('Excel.Application')    #COMオブジェクトを作成
-xl.Visible = true   #可視状態にする
+# xl.Visible = true   #可視状態にする
+xl.Visible = false   #不可視状態にする
 password = ''   #passwordの設定を行う。
-mojiTable = ("a".."z").to_a
-ary1 = ("A".."Z").to_a
-ary2 = ("0".."9").to_a
-mojiTable.concat(ary1).concat(ary2)
-mojiretu = []
-  mojiTable.each do |i|
-    mojiretu << i
-  end
-  mojiTable.each do |i|
-    mojiRetu = i
-    mojiTable.each do |i|
-      mojiretu << mojiRetu + i
-    end
-  end
-  mojiTable.each do |i|
-    mojiRetu = i
-    mojiTable.each do |i|
-      mojiRetu1 = mojiRetu + i
-      mojiTable.each do |i|
-        mojiretu <<= mojiRetu1 + i
-      end
-    end
-  end
+#mojiTable = ("a".."z").to_a
+#ary1 = ("A".."Z").to_a
+#ary2 = ("0".."9").to_a
+#$mojiTable.concat(ary1).concat(ary2)
+mojiTable = [('0'..'9'),('A'..'Z'),('a'..'z')].map{|c| c.to_a}.flatten
+$mojiKeta = 4
+$mojiIndex = 0
+atest = '3'
+atest << '1'
+puts atest
+p atest
+#-----------------------------------ここからコメント
+  mojiretu = []
+#  mojiTable.each do |i|
+#    mojiretu << i
+#  end
+#  mojiTable.each do |i|
+#    mojiRetu = i
+#    mojiTable.each do |i|
+#      mojiretu << mojiRetu + i
+#    end
+#  end
+#  mojiTable.each do |i|
+#    mojiRetu = i
+#    mojiTable.each do |i|
+#      mojiRetu1 = mojiRetu + i
+#      mojiTable.each do |i|
+#        mojiretu << mojiRetu1 + i
+#      end
+#    end
+#  end
+#-----------------------------------ここまでコメント
+p mojiretu
+exit(0)
 #fnct
-#p mojiretu
   i = 0
 begin
   #ファイルを開く
@@ -37,14 +48,23 @@ begin
 rescue
   puts "rescue‐start"
 #  password = 'cs'
+#  endFlg, password = makestring(mojiKeta, password)
+#  puts mojiretu[i]
+#  retry if endFlg 
   password = mojiretu[i]
-  puts mojiretu[i]
   i += 1
-  retry
+  retry 
 ensure
   xl.Quit
 end
-
+#def makeString(ketasu, mojiretu)
+#  if mojiIndex < mojiTable.size
+#    mojiretu  = mojiretu.chop! + mojiTable[mojiIndex]
+#  end
+#    mojiIndex += 1
+#  flg = true if mojiretu.size > ketasu
+#  return flg, mojiretu
+#end
 #def fnct
 #  mojiTable.each do |i|
 #    mojiretu << i
