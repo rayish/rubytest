a6503c36fed42d29fba20fdfd88890ba76859163
Haru.E <nobuch@beanbread.com>
2017-07-24 06:44:29 +0900

    siken3 commit

--- test/sikentest.rb
+++ test/sikentest.rb
@@ -11,7 +11,7 @@ end
 kingaku = Array.new(q,0)
 for i in (0...q)
     string = gets.chomp!
-    for j in (0...s) 
+    for j in (0...s)
         if /^#{string}/ =~ str[j]
             kingaku[i] += kin[j]
         end
@@ -24,7 +24,27 @@ input_lines = gets.chomp!.split(" ").map{|e| e.to_i}
 str = {}
 for i in (0...input_lines[0])
     lines = gets.chomp!.split(" ")
-    if str.has_key?(lines[0]) 
+    if str.has_key?(lines[0])
+        str[lines[0]] += lines[1].to_i
+    else
+        str[lines[0]] = lines[1].to_i
+    end
+end
+for i in (0...input_lines[1])
+    kingaku = 0
+    string = gets.chomp!
+    str.each_key do |key|
+        if /^#{string}/ =~ key
+            kingaku += str[key]
+        end
+    end
+    puts kingaku
+end
+input_lines = gets.chomp!.split(" ").map(&:to_i)
+str = {}
+for i in (0...input_lines[0])
+    lines = gets.chomp!.split(" ")
+    if str.has_key?(lines[0])
         str[lines[0]] += lines[1].to_i
     else
         str[lines[0]] = lines[1].to_i
